d0ee701772592c1abafb4bccf98a5bbc4fbb490a
PhilEllis <philippamacnab@hotmail.com>
2023-02-04 20:59:07 +0000

    Quiz Questions

---
+++ assets/js/script.js
@@ -0,0 +1,102 @@
+var spQuestions = [
+    {
+        question: "What planet is closest to the Sun?",
+        answers: {
+			a: 'Venus',
+			b: 'Mercury',
+			c: 'Mars',
+            d: 'Earth'
+		},
+		correctAnswer: 'b'
+	},
+    {
+		question: "Which planet has the highest mountain in our solar system, a volcano named Olympus Mons?",
+		answers: {
+			a: 'Mars',
+			b: 'Jupiter',
+			c: 'Neptune',
+			d: 'Earth'
+		},
+		correctAnswer: 'a'
+	},
+	{
+		question: "Which planet has organic life?",
+		answers: {
+			a: 'Saturn',
+			b: 'Mars',
+			c: 'Earth',
+			d: 'Uranus'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "Which planet is the brightest in our solar system?",
+		answers: {
+			a: 'Mars,
+			b: 'Neptune',
+			c: 'Saturn',
+			d: 'Venus''
+		},
+		correctAnswer: 'd'
+	},
+	{
+		question: "Which planet is Nicknamed the Icy planet as it is the coldest one?",
+		answers: {
+			a: 'Neptune',
+			b: 'Saturn',
+			c: 'Uranus',
+			d: 'Mars'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "Which planet is 5th from the sun and the biggest in our solar system?",
+		answers: {
+			a: 'Saturn',
+			b: 'Mars',
+			c: 'Mercury',
+			d: 'Jupiter'
+		},
+		correctAnswer: 'd'
+	},
+	{
+		question: "Which planet is the smallest planet?",
+		answers: {
+			a: 'Mercury',
+			b: 'Earth',
+			c: 'Neptune',
+			d: 'Jupiter'
+		},
+		correctAnswer: 'a'
+	},
+	{
+		question: "Which planet has outer rings made of dust and ice chunks?",
+		answers: {
+			a: 'Venus',
+			b: 'Saturn',
+			c: 'Uranus',
+			d: 'Jupiter'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "Which planet is the hottest planet?",
+		answers: {
+			a: 'Mercury',
+			b: 'Mars',
+			c: 'Venus',
+			d: 'Jupiter'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "Which planet is furthest from the sun?",
+		answers: {
+			a: 'Neptune',
+			b: 'Saturn',
+			c: 'Uranus',
+			d: 'Mercury'
+		},
+		correctAnswer: 'a'
+	},
+];
